86c8598e196e959791746121f1c36bd101fa1ca9
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-11-01 21:42:50 +0800

    add loading for brands, categories and sizes

---
+++ store-admin/app/(pages)/(dashboard)/brands/loading.tsx
@@ -0,0 +1,47 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
+import { Skeleton } from "@/app/components/ui/skeleton";
+
+const Loading = () => {
+  const brands = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="flex justify-normal flex-col gap-4">
+      <div className="flex flex-col md:flex-row justify-between gap-4">
+        <Skeleton className="w-32 h-10 ml-auto" />
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Slug</TableHead>
+            <TableHead className="w-3/12">Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {brands.map((brand) => (
+            <TableRow key={brand}>
+              <TableCell>
+                <Skeleton className="w-full h-6" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="w-full h-6" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="w-full h-6" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default Loading;
